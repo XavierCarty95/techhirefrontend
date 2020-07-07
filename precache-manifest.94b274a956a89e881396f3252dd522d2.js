5d422be6fe14fffd1fdc67b50b3489d8cdf5b17a
Xavier Carty <xaviercarty@gmail.com>
2020-07-07 10:27:04 -0400

    Updates

---
+++ precache-manifest.94b274a956a89e881396f3252dd522d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfc4c93d14f215897e4dc39337ced5c",
+    "url": "/techhirefrontend/index.html"
+  },
+  {
+    "revision": "1b0e0ab5c6b0b34e6a2e",
+    "url": "/techhirefrontend/static/css/main.4b5590c1.chunk.css"
+  },
+  {
+    "revision": "7e2b6f7a9295e7bf7bcd",
+    "url": "/techhirefrontend/static/js/2.3321e22c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/techhirefrontend/static/js/2.3321e22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0e0ab5c6b0b34e6a2e",
+    "url": "/techhirefrontend/static/js/main.302e86a2.chunk.js"
+  },
+  {
+    "revision": "8f3b8cbe2ab48bcb7884",
+    "url": "/techhirefrontend/static/js/runtime-main.c3ab3602.js"
+  }
+]);
